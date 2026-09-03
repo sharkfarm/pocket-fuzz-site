1d44c8146244ffdc0ec978530f7900bd64922362
Your Name <sharkfarm@gmail.com>
2026-09-02 23:05:16 -0600

    fix pixel connection

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import MetaPixel from "./meta-pixel";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,23 +30,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-
-              fbq('init', '1615201226612136');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
+        <MetaPixel />
 
         {children}
 
---
+++ app/meta-pixel.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState } from "react";
+
+export default function MetaCheckoutButton() {
+  const [submitting, setSubmitting] = useState(false);
+
+  function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+
+    if (submitting) return;
+
+    setSubmitting(true);
+
+    if (typeof window !== "undefined" && typeof window.fbq === "function") {
+      window.fbq("track", "InitiateCheckout");
+    }
+
+    const form = event.currentTarget.form;
+
+    setTimeout(() => {
+      form?.requestSubmit();
+    }, 400);
+  }
+
+  return (
+    <button
+      type="submit"
+      onClick={handleClick}
+      disabled={submitting}
+      className="w-full rounded-lg bg-red-600 px-6 py-4 font-black uppercase tracking-wide hover:bg-red-500 disabled:opacity-60"
+    >
+      {submitting ? "Opening Venmo..." : "Continue to Venmo"}
+    </button>
+  );
+}
